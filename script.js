e44e38b24396282f56a8bc61144adcd86eba1396
caio-chcn <caioh.mec@gmail.com>
2022-01-27 19:00:10 -0300

    terceiro desafio

---
+++ script.js
@@ -0,0 +1,19 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+let contador= 1;
+for(contador = 1;contador <=10;contador +=1){
+console.log(contador)
+}
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+while(10>=contador){
+    console.log(contador)
+    contador +=1;
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+let ate=1
+for(ate = 1;ate >=100;ate+=1){
+    console.log(ate)
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
